076659646b19e76261fa161aea5c204a15c98e9c
Guy Marshall <guymarshall9@gmail.com>
2022-11-23 22:29:19 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static ArrayList<Integer> getFactors(int number) {
+        ArrayList<Integer> factors = new ArrayList<>();
+
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                factors.add(i);
+            }
+        }
+
+        return factors;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number to check its factors: ");
+        int userInput = scanner.nextInt();
+        ArrayList<Integer> result = getFactors(userInput);
+        System.out.printf("Factors of %d:%n" +
+                "%s", userInput, result);
+    }
+}
