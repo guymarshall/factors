db0b534220b3ef51dae8b102388df26e4f7b2348
Guy Marshall <guymarshall9@gmail.com>
2022-11-24 18:37:38 +0000

    Add half stop to prevent checking for factors above 50% of number

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class Main {
     public static ArrayList<BigInteger> getFactors(BigInteger number) {
         ArrayList<BigInteger> factors = new ArrayList<>();
+        BigInteger halfNumber = number.divide(BigInteger.TWO);
 
-        for (BigInteger i = BigInteger.ONE; i.compareTo(number) <= 0; i = i.add(BigInteger.ONE)) {
+        for (BigInteger i = BigInteger.ONE; i.compareTo(halfNumber) <= 0; i = i.add(BigInteger.ONE)) {
             if (number.mod(i).equals(BigInteger.ZERO)) {
                 factors.add(i);
             }
