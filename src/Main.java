223cd7b1c73d9606d439bff937dbd8e648cf824f
Guy Marshall <guymarshall9@gmail.com>
2022-11-24 18:30:06 +0000

    Implement getFactors with BigInteger

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,24 @@
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    static ArrayList<Integer> getFactors(int number) {
-        ArrayList<Integer> factors = new ArrayList<>();
+//    static ArrayList<Integer> getFactors(int number) {
+//        ArrayList<Integer> factors = new ArrayList<>();
+//
+//        for (int i = 1; i <= number; i++) {
+//            if (number % i == 0) {
+//                factors.add(i);
+//            }
+//        }
+//
+//        return factors;
+//    }
+    public static ArrayList<BigInteger> getFactors(BigInteger number) {
+        ArrayList<BigInteger> factors = new ArrayList<>();
 
-        for (int i = 1; i <= number; i++) {
-            if (number % i == 0) {
+        for (BigInteger i = BigInteger.ONE; i.compareTo(number) <= 0; i = i.add(BigInteger.ONE)) {
+            if (number.mod(i).equals(BigInteger.ZERO)) {
                 factors.add(i);
             }
         }
@@ -17,8 +29,8 @@ public class Main {
         //TODO convert function to BigInteger for larger numbers
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number to check its factors: ");
-        int userInput = scanner.nextInt();
-        ArrayList<Integer> result = getFactors(userInput);
+        BigInteger userInput = scanner.nextBigInteger();
+        ArrayList<BigInteger> result = getFactors(userInput);
         System.out.printf("Factors of %d:%n" +
                 "%s", userInput, result);
     }
