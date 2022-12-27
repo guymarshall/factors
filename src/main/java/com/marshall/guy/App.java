421be6d7ce15ebe4e681a0811721ed46c0350844
Guy Marshall <guymarshall9@gmail.com>
2022-12-27 21:51:08 +0000

    Convert to VS Code

--- src/main/java/com/marshall/guy/App.java
+++ src/main/java/com/marshall/guy/App.java
@@ -1,24 +1,32 @@
+package com.marshall.guy;
+
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
-    public static ArrayList<BigInteger> getFactors(BigInteger number) {
+public class App 
+{
+    public static ArrayList<BigInteger> getFactors(BigInteger number)
+    {
         ArrayList<BigInteger> factors = new ArrayList<>();
         BigInteger halfNumber = number.divide(BigInteger.TWO);
 
-        for (BigInteger i = BigInteger.TWO; i.compareTo(halfNumber) <= 0; i = i.add(BigInteger.ONE)) {
-            if (number.mod(i).equals(BigInteger.ZERO)) {
+        for (BigInteger i = BigInteger.TWO; i.compareTo(halfNumber) <= 0; i = i.add(BigInteger.ONE))
+        {
+            if (number.mod(i).equals(BigInteger.ZERO))
+            {
                 factors.add(i);
             }
         }
 
         return factors;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number to check its factors: ");
         BigInteger userInput = scanner.nextBigInteger();
+        scanner.close();
         ArrayList<BigInteger> result = getFactors(userInput);
         System.out.printf("Factors of %d:%n" +
                 "%s", userInput, result);
